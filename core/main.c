6276ab076f1f6b505b67593655ec541ba7d64982
Robin Bron <robin@finwo.nl>
2017-01-24 09:38:00 +0100

    Added basic argument parsing

--- core/main.c
+++ core/main.c
@@ -1,9 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "messages.h"
 
 int main(int argc, char **argv)
 {
 
-  print_usage(*argv);
-  return 0;
+  unsigned char flags = 0;
+  char *driver = "filesystem";
+  int opt, port = 8080;
   
+  while((opt=getopt(argc, argv, "hp:d:"))!=-1) {
+    switch(opt) {
+      case 'd':
+        driver = optarg;
+        break;
+      case 'h':
+        print_usage(*argv);
+        exit(0);
+      case 'p':
+        port = atoi(optarg);
+        break;
+      default:
+        print_usage(*argv);
+        exit(1);
+    }
+  }
+  
+  return 0;
 }
